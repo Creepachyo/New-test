9f6e1a150417f5a4e4ee041586199d86ab375b58
Creepachyo <art49799z@gmail.com>
2026-03-15 11:17:37 +0300

    First commit

---
+++ Untitled-1.py
@@ -0,0 +1,122 @@
+
+from pygame import *
+
+window = display.set_mode((700, 500))
+display.set_caption('Maze')
+background = transform.scale(image.load("background.jpg"), (700, 500))
+
+#создай окно игры
+game = True
+
+clock = time.Clock()
+
+speed = 4
+x1 = 20
+y1 = 400
+ 
+x2 = 600
+y2 = 300
+
+x3 = 600
+y3 = 400
+
+FPS = 60
+
+mixer.init()
+mixer.music.load('jungles.ogg')
+mixer.music.play()
+
+font.init()
+font = font.Font(None, 70)
+defeat = font.render('Поражение', True, (180, 0, 0))
+
+class GameSprtite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.side = 'left'
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprtite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        
+        if keys_pressed[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys_pressed[K_RIGHT] and self.rect.x < 695:
+            self.rect.x += self.speed
+        if keys_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y <= 400:
+           self.rect.y += self.speed
+
+class Enemy(GameSprtite):
+    def update(self):
+        if self.rect.x <= 470:
+            self.side = 'right'
+        if self.rect.x >= 635:
+            self.side = 'left'
+        if self.side == 'left':
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+
+class Wall(sprite.Sprite):
+    def __init__(self, color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_height):
+        super().__init__()
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
+        self.width = wall_width
+        self.height = wall_height
+        self.image = Surface((self.width, self.height))
+        self.image.fill((color_1, color_2, color_3))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+player = Player('hero.png', x1, y1, speed)
+enemy = Enemy("cyborg.png", x2, y2, speed)
+finish = Player("treasure.png", x3, y3, speed)
+wall = Wall(100, 100, 100, 150, 0, 10, 350)
+wall1 = Wall(100, 100, 100, 250, 100, 10, 500)
+wall2 = Wall(100, 100, 100, 250, 100, 150, 10)
+
+end = False
+
+while game:
+ 
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if end != True:
+        window.blit(background, (0, 0))
+
+        player.update()
+        enemy.update()
+
+        player.reset()
+        enemy.reset()
+        finish.reset()
+        
+        wall.draw_wall()
+        wall1.draw_wall()
+        wall2.draw_wall()
+
+        if sprite.collide_rect(player, enemy) or sprite.collide_rect(player, wall) or sprite.collide_rect(player, wall1) or sprite.collide_rect(player, wall2):
+            window.blit(defeat, (200, 200))
+            end = True
+
+    display.update()
+    clock.tick(FPS)
+
+quit()
---
+++ shooter_game.py
@@ -0,0 +1,191 @@
+#Создай собственный Шутер!
+from pygame import *
+from random import randint
+from time import time as timer
+import sys 
+
+mixer.init()
+# mixer.music.load('space.ogg')
+# mixer.music.play()
+try:
+    firesound = mixer.Sound('fire.ogg')
+except FileNotFoundError:
+    sys.exit('Не найдены файлы игры')
+
+win_wid = 700
+win_hei = 500
+
+score = 0
+
+lost_NPC = 0
+
+max_lost = 5
+
+max_score = 10
+
+health = 5
+
+clip = 5
+
+flag_time = False
+
+font.init()
+main_font = font.SysFont('Arial', 40)
+defeat_font = font.SysFont('Arial', 100)
+defeat = defeat_font.render('Поражение', True, (255, 0, 0))
+win_font = font.SysFont('Arial', 80)
+win = win_font.render('Победа', True, (0, 255, 0))
+
+FPS = 60
+
+clock = time.Clock()
+
+window = display.set_mode((win_wid, win_hei))
+display.set_caption('Space war')
+background = transform.scale(image.load('galaxy.jpg'), (win_wid, win_hei))
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+
+        keys = key.get_pressed()
+
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_wid - 80:
+            self.rect.x += self.speed
+
+    def fire(self):
+        
+        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 15, 15)
+        bullets.add(bullet)
+    
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+
+        global lost_NPC
+
+        if self.rect.y >= win_hei:
+            self.rect.x = randint(80, win_wid - 80)
+            self.rect.y = randint(0, 0)
+            lost_NPC += 1
+        # if lost_NPC == max_lost:
+            # print('lose')
+
+class Bullet(GameSprite):
+
+    def update(self):
+        self.rect.y -= 15
+
+        if self.rect.y < 0:
+            self.kill()
+
+player = Player('rocket.png' , 5, win_hei - 110, 80, 100, 15)
+enemies = sprite.Group()
+asteroids = sprite.Group()
+
+for i in range(1,5):
+    enemy = Enemy('ufo.png', randint(80 , win_wid - 80), -40, 80, 50, randint(1,2))
+    enemies.add(enemy)
+
+for i in range(1, 4):
+    asteroid = Enemy('asteroid.png', randint(40 , win_wid - 40), randint(40 , win_hei - 40), 40, 40, randint(1 ,2))
+    asteroids.add(asteroid)
+    
+bullets = sprite.Group()
+
+finish = False
+
+run = True 
+
+while run:
+
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                if clip > 0 and flag_time == False:
+                    player.fire()
+                    firesound.play()
+                    clip -= 1
+                else:
+                    last_time = timer()
+                    flag_time = True
+            
+                    
+    if finish == False:
+        window.blit(background, (0, 0))
+
+        text_score = main_font.render(f'Счёт:{score}', True, (255, 255, 255))
+        lost_NPC_score = main_font.render(f'Пропущено:{lost_NPC}', True, (255, 255, 255))
+        window.blit(text_score, (5, 20))
+        window.blit(lost_NPC_score, (5, 50))
+
+        if health >= 3:
+            color = (0, 255, 0)
+        elif health == 2:
+            color = (255, 255, 0)
+        elif health == 1:
+            color = (255, 0, 0)
+           
+        text_health = main_font.render(f'Здоровье:{health}', True, color)
+        window.blit(text_health, (5, 80))
+
+        player.update()
+        enemies.update()
+        bullets.update()
+        asteroids.update()
+
+        player.reset()
+        enemies.draw(window)
+        bullets.draw(window)
+        asteroids.draw(window)
+
+        if flag_time == True:
+            now_time = timer()
+            if now_time - last_time < 3:
+                reload_text = main_font.render('Перезарядка', True, (255, 255, 255))
+                window.blit(reload_text, (5, 110))
+            else:
+                flag_time = False
+                clip = 5
+
+        collides = sprite.groupcollide(enemies, bullets, True, True)
+
+        for i in range(len(collides)):
+            enemy = Enemy('ufo.png', randint(80 , win_wid - 80), -40, 80, 50, randint(1,2))
+            enemies.add(enemy)
+
+        if sprite.spritecollide(player, asteroids, True):
+            health -= 1
+        elif sprite.spritecollide(player, enemies, True):
+            health -= 5
+
+        for i in collides:
+            score += 1
+            print(i)
+        
+        if lost_NPC == max_lost or health <= 0:
+            window.blit(defeat, (150, 200))
+            finish = True
+        elif score == max_score:
+            window.blit(win, (250, 200))
+            finish = True
+
+        display.update()
+    clock.tick(FPS)
+    
